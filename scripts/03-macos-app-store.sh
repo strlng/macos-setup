c285760e7d2d81457c190285d4c07190c938b77d
Sterling Anderson <s@sterlinganderson.net>
2018-09-24 10:08:28 -0500

    updates

--- scripts/03-macos-app-store.sh
+++ scripts/03-macos-app-store.sh
@@ -3,33 +3,29 @@
 # install mas-cli to install from the Mac App store
 brew install mas
 
-# get Apple ID
-echo ""
-echo "Enter AppleID to signin to Mac App Store:"
-read -p "  AppleID (john@foo.com): " APPLEID
-
-# make sure signed into Mac App Store
-mas signin $APPLEID
-
 # install macos apps
-mas install 1289583905 # Pixelmator Pro
-mas install 904280696 # Things3
-mas install 409183694 # Keynote
-mas install 409907375 # Remote Desktop
-mas install 931657367 # Calcbot
-mas install 441258766 # Magnet
-mas install 682658836 # GarageBand
-mas install 451907568 # Paprika Recipe Manager
-mas install 411643860 # DaisyDisk
-mas install 1333542190 # 1Password 7
-mas install 557168941 # Tweetbot
-mas install 975937182 # Fantastical 2
-mas install 409203825 # Numbers
-mas install 497799835 # Xcode
-mas install 409201541 # Pages
-mas install 1091189122 # Bear
-mas install 408981434 # iMovie
-mas install 1148868081 # iStat View
-mas install 1082624744 # Gifox
-mas install 803453959 # Slack
-mas install 924726344 # Deliveries
+mas install 1107421413 #1Blocker (1.4.3)
+mas install 1333542190 #1Password 7 (7.1.2)
+mas install 931657367 #Calcbot (1.0.7)
+mas install 587512244 #Kaleidoscope (2.2.2)
+mas install 682658836 #GarageBand (10.3.1)
+mas install 451907568 #Paprika Recipe Manager (2.2.0)
+mas install 497799835 #Xcode (10.0)
+mas install 1082624744 #Gifox (1.6.1)
+mas install 1384080005 #Tweetbot (3.1)
+mas install 924726344 #Deliveries (3.0.9)
+mas install 1037126344 #Apple Configurator 2 (2.7.1)
+mas install 1289583905 #Pixelmator Pro (1.1.4)
+mas install 441258766 #Magnet (2.4)
+mas install 442168834 #SiteSucker (2.11.8)
+mas install 1148868081 #iStat View (3.04)
+mas install 1295203466 #Microsoft Remote Desktop (10.2.1)
+mas install 409201541 #Pages (7.2)
+mas install 803453959 #Slack (3.3.2)
+mas install 975937182 #Fantastical 2 (2.5.4)
+mas install 411643860 #DaisyDisk (4.6.5)
+mas install 409203825 #Numbers (5.2)
+mas install 409183694 #Keynote (8.2)
+mas install 409907375 #Remote Desktop (3.9)
+
+sudo xcodebuild -license accept
